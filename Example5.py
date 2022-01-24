014fe69d126f972b8f2cabd809851a59c52ca199
Shahin1723 <79781718+Shahin1723@users.noreply.github.com>
2022-01-24 14:26:04 +0530

    Add files via upload
    strings

---
+++ Example5.py
@@ -0,0 +1,11 @@
+str1 = "Hello"
+str2 = "Shahin    "
+print(str1 + str2)
+res = print(str1 +' '+ str2)
+a = str2.strip()
+print(a)
+x = len(str1)
+print(x)
+print(str1[1:3])
+c = str2.replace("Shahin","Fathima")
+print(c)
