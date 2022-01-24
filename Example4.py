a77003042e46b5c9fe684b67e1e63f6c08be8322
Shahin1723 <79781718+Shahin1723@users.noreply.github.com>
2022-01-24 14:25:43 +0530

    Add files via upload
    loops & iterations

---
+++ Example4.py
@@ -0,0 +1,16 @@
+n = 5
+while (n > 0):
+  print(n)
+  n = n-1
+#print("End")
+#while True :
+#    line = input("Enter your name :")
+#    if (line == 'Shahin'):
+#        break;
+#print("end")
+a = 0
+for i in [1,2,3]:
+    if ( i > a):
+        great_num = i
+        a = i
+print("Greatest is :" , i )
