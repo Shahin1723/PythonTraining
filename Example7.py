3e042421132cb9ab3e51a9e1e84b735614f12e47
Shahin1723 <79781718+Shahin1723@users.noreply.github.com>
2022-01-24 15:36:09 +0530

    Add files via upload
    Lists

---
+++ Example7.py
@@ -0,0 +1,15 @@
+friends =['b','c','a']
+for friend in friends:
+  print("Hello", friend)
+print(len(friends))
+a = [1,2]
+b = [3,4]
+c = a + b
+print(c)
+friends.sort()
+print(friends)
+friends.reverse()
+print(friends)
+res = 'hello shahin'
+y=res.split()
+print(y)
